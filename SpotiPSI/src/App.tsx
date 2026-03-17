b970b064834ad3b140e4c22cfeb946cdff7899be
23 <livnatliron@gmail.com>
2026-03-17 19:24:30 +0200

    function which gets array of data from the server

---
+++ SpotiPSI/src/App.tsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const URL: string = 'http://127.0.0.1:5001/api/'
+
+
+interface Song {
+    id: string,
+    name: string,
+    artist: string,
+    album: string
+}
+
+
+const App: React.FC = () => {
+
+    const [allSongs, setAllSongs] = useState<Song[]>([])
+
+    /**
+     * 
+     * @param route the route to the array we want to get. Either "songs", "playlists" or "favorites"
+     * @param setFunction a set function that updates the array
+     */
+    const fetchSongs = async <T,>(route: "songs" | "playlists" | "favorites", setFunction: React.Dispatch<React.SetStateAction<T[]>>) => {
+
+        const urlWithRoute = `${URL}/${route}`
+        try {
+
+            //gets the data from the server
+            const response = await fetch(urlWithRoute);
+            const data = await response.json();
+
+            setFunction(() => data);
+
+        } catch (error) {
+
+            console.log(error);
+
+        }
+    }
+
+
+    return (
+        <div>
+
+        </div>
+    )
+
+
+
+
+
+}
+
+
+export default App
